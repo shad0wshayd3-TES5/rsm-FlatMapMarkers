e23d8a6b9d242da8f4eed452f77fc6b42bf1f34c
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-20 23:10:30 -0700

    refactor: wip xmake work

--- src/PCH.h
+++ src/PCH.h
@@ -1,29 +1,12 @@
 #pragma once
-#pragma warning(disable : 4702)
-
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-#include <spdlog/sinks/basic_file_sink.h>
-
-#define DLLEXPORT __declspec(dllexport)
-
 using namespace std::literals;
 
-namespace logger = SKSE::log;
-
-namespace stl
-{
-	using namespace SKSE::stl;
-}
-
-#ifdef SKYRIM_AE
-# define OFFSET(se, ae) ae
+#ifdef SKYRIM_SUPPORT_AE
+#	define OFFSET(se, ae) ae
 #else
-# define OFFSET(se, ae) se
+#	define OFFSET(se, ae) se
 #endif
-
-#include "Version.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,111 +1,42 @@
 #include "Events.h"
 #include "Hooks.h"
 
-#ifdef SKYRIM_AE
-extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []()
-{
-	SKSE::PluginVersionData v;
-	v.PluginVersion(Version::MAJOR);
-	v.PluginName(Version::PROJECT);
-	v.AuthorName("shad0wshayd3");
-	v.UsesAddressLibrary();
-	v.UsesUpdatedStructs();
-	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-
-	return v;
-}();
-#else
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
-{
-	a_info->infoVersion = SKSE::PluginInfo::kVersion;
-	a_info->name = Version::PROJECT.data();
-	a_info->version = Version::MAJOR;
-
-	if (a_skse->IsEditor())
-	{
-		logger::critical("Loaded in editor, marking as incompatible"sv);
-		return false;
-	}
-
-	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_39)
-	{
-		logger::critical(FMT_STRING("Unsupported runtime version {}"sv), ver.string());
-		return false;
-	}
-
-	return true;
-}
-#endif
-
 namespace
 {
-	void InitializeLog()
+	void MessageCallback(SKSE::MessagingInterface::Message* a_msg)
 	{
-		auto path = logger::log_directory();
-		if (!path)
-		{
-			stl::report_and_fail("Failed to find standard logging directory"sv);
-		}
-
-		*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
-		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-
-		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-		log->set_level(spdlog::level::info);
-		log->flush_on(spdlog::level::info);
-
-		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("[%^%l%$] %v"s);
-
-		logger::info(FMT_STRING("{:s} v{:s}"), Version::PROJECT, Version::NAME);
-	}
-
-	void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
-	{
-		if (!a_msg)
-		{
-			return;
-		}
-
 		switch (a_msg->type)
 		{
-			case SKSE::MessagingInterface::kDataLoaded:
-				{
-					if (auto UI = RE::UI::GetSingleton())
-					{
-						UI->AddEventSink(Events::MenuOpenCloseEventHandler::GetSingleton());
-						logger::info("Registered MenuOpenCloseEventHandler."sv);
-					}
-				}
-				break;
+		case SKSE::MessagingInterface::kPostLoad:
+		{
+			Hooks::Install();
+			break;
+		}
+		case SKSE::MessagingInterface::kDataLoaded:
+		{
+			if (auto UI = RE::UI::GetSingleton())
+			{
+				UI->AddEventSink(Events::MenuOpenCloseEventHandler::GetSingleton());
+				SKSE::log::info("Registered MenuOpenCloseEventHandler."sv);
+			}
+		}
+		break;
 		}
 	}
 }
 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-	InitializeLog();
-	logger::info(FMT_STRING("{:s} loaded"), Version::PROJECT);
-
 	SKSE::Init(a_skse);
 	SKSE::AllocTrampoline(1 << 4);
 
 	if (!Settings::LoadSettings())
 	{
-		logger::error("Failed to load settings."sv);
+		SKSE::log::error("Failed to load settings."sv);
 		return false;
 	}
 
-	const auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging || !messaging->RegisterListener(MessageHandler))
-	{
-		logger::error("Failed to register MessageHandler."sv);
-		return false;
-	}
-
-	Hooks::Install();
+	SKSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 
 	return true;
 }
